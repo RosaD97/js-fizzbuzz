af94f52735bc92af19d7affcf09791b748ee7de3
RosaD97 <r960595@gmail.com>
2023-02-23 16:54:31 +0100

    Fizz e Buzz fixato

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@
 
 // Numerazione da 1 a 100
 for(let i = 1; i <= 100; i++ ){
-    if ((i % 3 == 0) && (i % 5 == 0))
-    console.log('FizzBuzz');
-     else if(i % 3 == 0){
-            console.log('fizz');
-        }
-    else if(i % 5 == 0){
-            console.log('Buzz');
-        }
+    if ((i % 3 == 0) && (i % 5 == 0)){
+        let i = ('FizzBuzz');
         console.log(i);
-
+    }else if(i % 3 == 0){
+        console.log('fizz');
+    }else if(i % 5 == 0){
+        console.log('Buzz');
+    }else {
+        console.log(i);
+    }
+        const main = document.querySelector(".bigContainer")
+        const div = document.createElement('div');
+        div.append(i);
+        main.append(div);
 }
+
 
